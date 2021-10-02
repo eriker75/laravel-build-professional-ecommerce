34db103c54099fad40b8634d269e16ef3c777f81
eriker75 <67813946+eriker75@users.noreply.github.com>
2021-10-02 10:15:28 -0400

    HTTP methods comment
    HTTP methos utility description

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/* 6 Types of Routes methods
+    GET     Obtain data
+    POST    Insert data in the database
+    PUT     Update data in database
+    DELETE  Delete data from database
+    PATCH   Updata a single field in the database
+    OPTIONS Updata a single field in the database
 */
 
 Route::get('/', function () {
